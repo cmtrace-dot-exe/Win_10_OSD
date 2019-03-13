169a724e3753d37dc45c02a4bdfc72006d231606
cmtrace-dot-exe <39200612+cmtrace-dot-exe@users.noreply.github.com>
2019-03-13 11:45:00 -0500

    Preps an OS drive's partitions for use with MBR2GPT
    This PowerShell script will eliminate all non-boot, active or system partitions < 1GB on your OS disk, then create and correctly ID a new recovery partition, making MBR2GPT happy.

--- MBR2GPT_CleanRecoveryPartition.ps1
+++ MBR2GPT_CleanRecoveryPartition.ps1
@@ -1,7 +1,8 @@
 ############################################################################################################
 ### Windows 10 Recovery Partition Correction Script
 ### olearym
-### Changelog: 	2018-05-13
+### Changelog: 	2018-05-13	- Original Upload
+### Changelog: 	2019-03-13	- Increased size of recovery partition & added GPT detection for ID application
 ############################################################################################################
 
 # Test for powershell version, populate $PSScriptRoot if version < 3 
@@ -11,21 +12,25 @@
 	$osPart = $(get-partition -driveletter $($env:SystemDrive).Trim(':'))
 	 
 # Set target size for new recovery partition
-	$rePartTargetSize = 472907776
+	#$rePartTargetSize = 525336576
+	$rePartTargetSize = 944766976
 
 # Remove all partitions on OS Drive that are missing boot, active & system flags, provided they are under 1GB in size.
 	get-partition -DiskNumber $osPart.DiskNumber | % {if ((-not $_.IsBoot) -and (-not $_.IsActive) -and (-not $_.IsSystem) -and ($_.Size -lt 1073741824)) {Remove-Partition -InputObject $_ -confirm:$false}}
-
+	
 # Resize OS Partition
-	Resize-partition -InputObject $osPart -Size $($(Get-PartitionSupportedSize -InputObject $osPart).SizeMax - $rePartTargetSize) 
+	Resize-partition -InputObject $osPart -Size $($(Get-PartitionSupportedSize -InputObject $osPart).SizeMax - $rePartTargetSize) -ErrorAction SilentlyContinue
 
 # Create and format new recovery partition
 	$rePartNew = $(New-Partition -DiskNumber $osPart.DiskNumber -usemaximumsize)
 	format-volume -partition $rePartNew -FileSystem NTFS -NewFileSystemLabel "Recovery" 
 
-# Apply correct ID number to new recovery partition
-	NEW-ITEM -Path "$PSScriptRoot" -Name "DISKPART.txt" -ItemType "File" -force | OUT-NULL
-	ADD-CONTENT -path "$PSScriptRoot\DISKPART.txt" -Value $('SELECT DISK ' + $($rePartNew.DiskNumber))
-	ADD-CONTENT -path "$PSScriptRoot\DISKPART.txt" -Value $('SELECT PARTITION ' + $($rePartNew.PartitionNumber))
-	ADD-CONTENT -path "$PSScriptRoot\DISKPART.txt" -Value "SET ID=27 OVERRIDE"
-	& DISKPART /S "$PSScriptRoot\DISKPART.txt"
+# Apply correct ID number to new recovery partition if MBR
+	if ($($(get-disk -Number $osPart.DiskNumber).PartitionStyle) -ne "GPT")
+	{
+		NEW-ITEM -Path "$PSScriptRoot" -Name "DISKPART.txt" -ItemType "File" -force | OUT-NULL
+		ADD-CONTENT -path "$PSScriptRoot\DISKPART.txt" -Value $('SELECT DISK ' + $($rePartNew.DiskNumber))
+		ADD-CONTENT -path "$PSScriptRoot\DISKPART.txt" -Value $('SELECT PARTITION ' + $($rePartNew.PartitionNumber))
+		ADD-CONTENT -path "$PSScriptRoot\DISKPART.txt" -Value "SET ID=27 OVERRIDE"
+		& DISKPART /S "$PSScriptRoot\DISKPART.txt"
+	}
